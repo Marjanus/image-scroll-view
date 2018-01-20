895acdb6403c7d37d575f306b407065ff2d31b1f
Marjanus <martynas.janusevicius@gmail.com>
2018-01-20 19:09:49 +0200

    Map over response images

--- src/app.js
+++ src/app.js
@@ -6,10 +6,12 @@ const keys = require('../keys');
 const API_KEY = keys.key;
 
 app.get('/', (req, res) =>
-
   axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=${API_KEY}&per_page=10&format=json&nojsoncallback=1`)
     .then(response => {
-      res.send(response.data);
+      const imagesData = response.data.photos.photo.map(item => {
+        return {title: item.title, src: `https://farm${item.farm}.staticflickr.com/${item.server}/${item.id}_${item.secret}.jpg`};
+      });
+      res.send(imagesData);
     })
     .catch(error => {
       console.log(error);
